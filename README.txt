8e2de347edde33236b1d076a777a18bfa6586d80
YaroslavW <doctor355@yahoo.com>
2018-04-22 13:00:01 +0300

    all is ready

---
+++ README.txt
@@ -0,0 +1,38 @@
+SNB Social Neworks Buttons for Mozilla Firefoz
+This extension is for Mozilla Firefox browser version  57.0 and more.
+Windows(32-bit or 64-bit).
+
+1. Download the extension to a separate folder on your computer.
+
+2. Download the extension from the official site. https://addons.mozilla.org/ru/firefox/addon/greasemonkey/
+
+3. Click on the extension icon and select "New user script". 
+The tab with the extension code will open.
+Removes everything from there.
+
+4. Copy everything from the file snb-firefox-social-networks-buttons.js 
+Copy everything from the file (Ctrl + A, Ctrl + C),
+and paste (Ctrl + V) it in place of the remote code of the tab.
+
+5. Click on the save icon, or Ctrl + S.
+
+Sometimes, you need permission to use pop-up windows for sites 
+on which you will use the extension.
+More information here: https://abcinblog.blogspot.com/2018/04/firefox-snb-social-neworks-buttons.html
+
+ Now the extension is fully operational and does not require 
+ any additional settings.
+ If necessary, you can disable it by moving this box.
+
+Now that you go to a page where there are no social networking buttons, 
+you can always quickly share it with your readers by 
+simply pressing 
+ Shift + G for Google Plus,
+ Shift + F for Facebook,
+ Shift + T for Twitter.
+
+FireFox: SNB Social Neworks Buttons.
+https://abcinblog.blogspot.com/2018/04/firefox-snb-social-neworks-buttons.html
+
+2017 -  All rights reserved.
+Created with love by Yaroslav Kolesnikov.
---
+++ snb-firefox-social-networks-buttons.js
@@ -0,0 +1,118 @@
+// ----------Куча комментариев----------------------------------------------
+//
+// ==UserScript==
+// @name LOADER
+// @namespace abcinblog.blogspot.ru
+// @version 0.01
+// 
+// @description Этот скрипт поможет вам загрузить картинку из Инстаграм - откроет на клик в удобном формате для скачивания!
+// @include https://www.facebook.com/*
+// @include https://m.facebook.com/*
+// @include https://www.instagram.com/*
+// @include https://twitter.com/*
+// @include https://mobile.twitter.com/*
+
+// ==/UserScript==
+document.addEventListener("keydown", function(e){
+  e = e || window.event;
+  if (e.shiftKey && e.keyCode == 65) {
+
+        var currentLocation = window.location.host;
+
+        function getFacebookUrl(){
+         var x = window.location.href;
+         location.href = x.replace(/www/,"m");
+       };
+
+       function geFacebookSrc(){
+         var bl = document.querySelector('._53mw');
+         var atr = bl.dataset.store;
+         atr = JSON.parse(atr);
+         location.href=atr.src; 
+       };
+
+
+
+
+      function getSrcInst(){
+
+        var src = document.querySelector('video');
+        if(!src){
+         var div = document.getElementsByTagName('div');
+         for( var i = 0; i < div.length; i++ ){
+          if(div[i].hasAttribute('role') && div[i].getAttribute('role') == 'dialog'){
+            var block = div[i];
+          } 
+        }
+    // For photos opened in own tabs
+    if(block == null) {
+     block= document.querySelector('._4rbun');
+     var urlInst = block.querySelector('img').src;
+     window.open(urlInst);
+    }
+
+    var elements = block.getElementsByTagName('img');
+    var source = elements[1].src;
+    window.open(source);
+    } else {
+      var src = document.querySelector('video').src;
+      window.open(src);
+    }
+    };
+
+
+    function twitterMedia(){
+      var el = document.querySelector(".Gallery-media");
+      if(el.innerHTML == ""){
+       var url = window.location.href;
+       var fl = url.indexOf("twitter");
+
+       var a = url.substring(0,fl);
+       var hrefTw = a + 'mobile.' + url.substring(fl);
+
+       window.open(hrefTw);
+       
+
+     } else {
+      var srcImg = el.querySelector('img').src;
+      var lastLetter = srcImg.indexOf(':large');
+      var url = srcImg.substring(0, lastLetter);
+      window.open(url);
+
+    }
+    };
+
+    function twitterMob(){
+      var bl = document.getElementById('react-root')
+      var sources = bl.querySelectorAll('source');
+      for(var i = 0; i < sources.length; i++){
+      	var src = sources[1].src;
+      }
+     
+      window.location.href = src;
+      };
+      
+
+    switch(currentLocation){
+
+      case 'www.instagram.com':
+      getSrcInst();
+      break;
+      case 'www.facebook.com':
+      getFacebookUrl();
+      break;
+      case 'm.facebook.com':
+      geFacebookSrc(); 
+      break;
+      case 'twitter.com':
+      twitterMedia(); 
+      break;
+      case 'mobile.twitter.com':
+      twitterMob(); 
+      break;       
+      default:
+      console.log("Sorry,it does not work here!");
+    }
+}
+return true;
+});
